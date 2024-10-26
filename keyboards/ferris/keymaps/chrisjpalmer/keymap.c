8e5928dcc086d450775c35fa4d99dd24294fcb7f
chrisjpalmer <chrisjpalmer@acme.com>
2024-10-26 17:29:19 +1100

    chris m keymap

--- keyboards/ferris/keymaps/chrisjpalmer/keymap.c
+++ keyboards/ferris/keymaps/chrisjpalmer/keymap.c
@@ -11,56 +11,105 @@ void keyboard_post_init_user(void) {
     debug_keyboard = true;
 }
 
+enum layers {
+    // The base layer for most english typing
+    _QWERTY,
+    // Special characters
+    _SPECIAL,
+    // Numpad on right hand
+    // Media keys on the left hand
+    _NUMBERS,
+    // Keys often required while using numbers on the right hand - plus, minus, etc.
+    // Function keys on the left hand
+    _NUMBERS_AUX,
+    // Arrow key layer WIP
+    _ARROWS,
+
+    // EPHEMERAL LAYERS
+    // The following layers are not in the same spirit as the rest of the board,
+    // and thus are more difficult to access in everyday use.
+    // The EPHEMERAL_HOME layer can be reached from the normal layers, and the
+    // other ephemeral layers can be reached from there.
+    _EPHEMERAL_HOME,
+};
+
+#define EP_HOME TO(_EPHEMERAL_HOME)
+#define OS_NUMX OSL(_NUMBERS_AUX)
+
+#define GUI_SLS LGUI_T(KC_SLSH)
+#define GUI_Z LGUI_T(KC_Z)
+#define ALT_X LALT_T(KC_X)
+#define ALT_DOT LALT_T(KC_DOT)
+
+
+// Defines a layout for auxiliary layers; the layout will be common on the
+// different layers, so it is better to only define it once.
+#define LAYOUT_SUPER( \
+    L01, L02, L03, L04, L05, R01, R02, R03, R04, R05, \
+    L06, L07, L08, L09, L10, R06, R07, R08, R09, R10, \
+    L11, L12, L13, L14, L15, R11, R12, R13, R14, R15, \
+                   L16, L17, R16, R17 \
+) LAYOUT( \
+    KC_ESC,  L02, L03, L04, L05, R01, R02, R03, R04, KC_BSPC, \
+    KC_TAB,  L07, L08, L09, L10, R06, R07, R08, R09, KC_ENT, \
+    KC_BTN1, L12, L13, L14, L15, R11, R12, R13, R14, R15, \
+               KC_LCTL, _______, R16, KC_LSFT \
+)
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _SPECIAL, _NUMBERS, _ARROWS);
+}
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x5_2(
-        KC_Q,           KC_W,           KC_E,           KC_R,         KC_T,                     KC_Y,           KC_U,         KC_I,              KC_O,             KC_P,              
-        LSFT_T(KC_A),   LT(5,KC_S),     LT(1,KC_D),     LT(3,KC_F),   KC_G,                     KC_H,           LT(4,KC_J),   LT(2,KC_K),        LT(6,KC_L),       LSFT_T(KC_SCLN),   
-        KC_Z,           LCTL_T(KC_X),   LALT_T(KC_C),   KC_V,         KC_B,                     KC_N,           KC_M,         LALT_T(KC_COMM),   LCTL_T(KC_DOT),   KC_SLSH,           
-                                                        CG_TOGG,      KC_BSPC,                  LT(7,KC_SPC),   KC_P1                                                                 
+    [_QWERTY] = LAYOUT(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        GUI_Z,   ALT_X,   KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, ALT_DOT, GUI_SLS,
+              LCTL_T(KC_ENT), LT(_SPECIAL, KC_BSPC),    LT(_NUMBERS, KC_SPC), KC_LSFT
     ),
-    [1] = LAYOUT_split_3x5_2(
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,            KC_TRNS,   KC_BTN1,   KC_WH_U,   KC_BTN2,   KC_TRNS,   
-        KC_TRNS,   KC_BTN2,   KC_NO,     KC_BTN1,   KC_TRNS,            KC_TRNS,   KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,   
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,            KC_TRNS,   KC_WH_L,   KC_WH_D,   KC_WH_R,   KC_TRNS,   
-                                         KC_TRNS,   KC_TRNS,            KC_TRNS,   KC_TRNS                                     
+
+    [_SPECIAL] = LAYOUT_SUPER(
+        _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_TILD, _______,
+        _______, KC_EXLM, KC_EQL,  KC_MINS, KC_QUOT,   KC_GRV,  KC_BSLS, KC_LCBR, KC_RCBR, _______,
+        _______, KC_PIPE, KC_PLUS, KC_UNDS, KC_DQUO,   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,
+                                   _______, _______,   _______, _______
     ),
-    [2] = LAYOUT_split_3x5_2(
-        KC_TRNS,   KC_TRNS,   KC_PGUP,   KC_TRNS,   KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,         KC_TRNS,        
-        KC_LEFT,   KC_UP,     KC_DOWN,   KC_RGHT,   KC_TRNS,            KC_TRNS,   KC_LGUI,   KC_NO,     LCTL(KC_LALT),   LCA(KC_LSFT),   
-        KC_TRNS,   KC_HOME,   KC_PGDN,   KC_END,    KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,         KC_TRNS,        
-                                         KC_TRNS,   KC_TRNS,            KC_TRNS,   KC_TRNS                                                
+
+    [_NUMBERS] = LAYOUT_SUPER(
+        _______, KC_MSTP, KC_MPRV, KC_MNXT, _______,   KC_UNDS, KC_7,    KC_8,    KC_9,    _______,
+        _______, KC_VOLD, KC_VOLU, KC_MPLY, _______,   KC_SPC,  KC_4,    KC_5,    KC_6,    _______,
+        _______, KC_BRID, KC_BRIU, KC_MUTE, _______,   KC_0,    KC_1,    KC_2,    KC_3,    OS_NUMX,
+                                   _______, _______,   _______, _______
     ),
-    [3] = LAYOUT_split_3x5_2(
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,                   KC_TRNS,   KC_UNDS,   KC_PIPE,   KC_QUOT,   KC_TRNS,   
-        KC_CIRC,   KC_ASTR,   KC_AMPR,   KC_NO,      KC_TRNS,                   KC_HASH,   KC_TILD,   KC_SLSH,   KC_DQUO,   KC_DLR,    
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,                   KC_TRNS,   KC_MINS,   KC_BSLS,   KC_GRV,    KC_TRNS,   
-                                         RGB_RMOD,   KC_TRNS,                   KC_TRNS,   RGB_MOD                                     
+
+    [_NUMBERS_AUX] = LAYOUT_SUPER(
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,    _______, _______, _______, _______, _______,
+        _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,    _______, KC_PLUS, KC_ASTR, KC_DOT,  _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F10,    KC_COLN, KC_MINS, KC_SLSH, KC_COMM, KC_NO,
+                                   _______, _______,   _______, _______
     ),
-    [4] = LAYOUT_split_3x5_2(
-        KC_TRNS,   KC_COLN,   KC_LT,     KC_GT,     KC_SCLN,            KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   
-        KC_LCBR,   KC_RCBR,   KC_LPRN,   KC_RPRN,   KC_AT,              KC_TRNS,   KC_NO,     KC_EQL,    KC_PLUS,   KC_PERC,   
-        KC_TRNS,   KC_EXLM,   KC_LBRC,   KC_RBRC,   KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   
-                                         KC_VOLD,   KC_TRNS,            KC_TRNS,   KC_VOLU                                     
+
+    [_ARROWS] = LAYOUT_SUPER(
+        _______, KC_WH_D, KC_MS_U, KC_WH_U, QK_RBT,    _______, _______, _______, _______, _______,
+        _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______, KC_BTN2, _______, _______, EP_HOME,   _______, _______, _______, _______, _______,
+                                   _______, _______,   _______, _______
     ),
-    [5] = LAYOUT_split_3x5_2(
-        KC_TRNS,   KC_TRNS,   KC_TRNS,         KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_F7,    KC_F8,   KC_F9,   KC_F10,   
-        KC_TRNS,   KC_NO,     LCTL(KC_LALT),   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_F4,    KC_F5,   KC_F6,   KC_F11,   
-        KC_TRNS,   KC_TRNS,   KC_TRNS,         KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_F1,    KC_F2,   KC_F3,   KC_F12,   
-                                               KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS                               
+
+    [_EPHEMERAL_HOME] = LAYOUT(
+        _______,     _______,   _______,          _______,        CG_TOGG,
+        _______,     _______,   _______,          _______,        _______,
+
+        _______,     _______,   _______,          _______,        _______,
+        _______,     _______,   _______,          _______,        _______,
+
+        _______,     _______,   _______,          _______,        _______,
+        _______,     _______,   _______,          _______,        QK_BOOT,
+
+        _______,        TO(_QWERTY),
+        TO(_SPECIAL),   _______
     ),
-    [6] = LAYOUT_split_3x5_2(
-        KC_PSLS,   KC_7,   KC_8,   KC_9,      KC_PPLS,                  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   
-        KC_0,      KC_1,   KC_2,   KC_3,      KC_PMNS,                  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_NO,     KC_TRNS,   
-        KC_PAST,   KC_4,   KC_5,   KC_6,      KC_PEQL,                  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   
-                                   KC_TRNS,   KC_TRNS,                  KC_TRNS,   KC_TRNS                                     
-    ),
-    [7] = LAYOUT_split_3x5_2(
-        KC_TRNS,   KC_TRNS,   KC_COLN,   KC_ESC,    KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS,           KC_TRNS,          KC_DEL,    
-        KC_TRNS,   KC_PERC,   KC_SLSH,   KC_ENT,    KC_TRNS,            DF(1),     KC_LGUI,   KC_TRNS,           KC_TRNS,          KC_TRNS,   
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_EXLM,   KC_TRNS,            DF(0),     KC_TRNS,   RALT_T(KC_COMM),   RCTL_T(KC_DOT),   QK_BOOT,   
-                                         KC_TRNS,   KC_TAB,             KC_NO,     KC_TRNS                                                    
-    )
 };
 // clang-format on
 static bool did_detect_mac = false;
